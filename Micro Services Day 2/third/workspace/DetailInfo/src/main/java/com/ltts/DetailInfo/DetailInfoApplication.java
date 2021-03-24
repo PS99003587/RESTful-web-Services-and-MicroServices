673a66b5347093c905385eca3d7ccd8ef7918af6
Kricha Sahu <78844495+PS99003587@users.noreply.github.com>
2021-03-24 12:19:10 +0530

    Detail Info

---
+++ Micro Services Day 2/third/workspace/DetailInfo/src/main/java/com/ltts/DetailInfo/DetailInfoApplication.java
@@ -0,0 +1,27 @@
+package com.ltts.DetailInfo;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class DetailInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetailInfoApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	//load balancing mechanism and service discovery
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+}
